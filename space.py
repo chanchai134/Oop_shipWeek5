6c2c8bbb109ccb120d45a62b08da5b6c6ea3cd0f
chanchai charnataparporn <pom51142@gmail.com>
2017-09-08 16:30:56 +0700

    world class add

--- space.py
+++ space.py
@@ -1,5 +1,5 @@
 import arcade
-from models import Ship
+from models import World, Ship
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
@@ -10,17 +10,16 @@ class SpaceGameWindow(arcade.Window):
  
         arcade.set_background_color(arcade.color.BLACK)
  
-        self.ship_posiotion = Ship(100,100)
         self.ship_sprite = arcade.Sprite('images/ship.png')
+        self.world = World(width, height)
     
     def on_draw(self):
         arcade.start_render()
         self.ship_sprite.draw()
         
     def update(self, delta):
-        ship = self.ship_posiotion
-        ship.update(delta)
-        self.ship_sprite.set_position(ship.x, ship.y)
+        self.world.update(delta)
+        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
         
  
 if __name__ == '__main__':
