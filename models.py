72653a5171f6698bc9e85c9e8ad419aafbd58524
chanchai charnataparporn <pom51142@gmail.com>
2017-09-08 17:17:57 +0700

    addgold

--- models.py
+++ models.py
@@ -1,11 +1,14 @@
 import arcade
 import arcade.key
+from random import randint
 class Model:
     def __init__(self, world, x, y, angle):
         self.world = world
         self.x = x
         self.y = y
         self.angle = 0
+    def hit(self, other, hit_size):
+        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
 class Ship(Model):
     DIR_HORIZONTAL = 0
     DIR_VERTICAL = 1
@@ -47,6 +50,9 @@ class World:
         self.world.on_key_press(key, key_modifiers)
     def update(self, delta):
         self.ship.update(delta)
+ 
+        if self.ship.hit(self.gold, 15):
+            self.gold.random_location()
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE:
             self.ship.switch_direction()
@@ -69,6 +75,9 @@ class Gold:
         self.world = world
         self.x = x
         self.y = y
+    def random_location(self):
+        self.x = randint(0, self.world.width - 1)
+        self.y = randint(0, self.world.height - 1)
 
 class Gold(Model):
     def __init__(self, world, x, y):
